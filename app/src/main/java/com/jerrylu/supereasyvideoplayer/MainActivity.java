86196855f97690aa2b15a0befec3313cf7b6c6fb
Jerry Lu <jerrylu.mail@qq.com>
2018-02-17 00:01:28 +0800

    Able to play video now

--- app/src/main/java/com/jerrylu/supereasyvideoplayer/MainActivity.java
+++ app/src/main/java/com/jerrylu/supereasyvideoplayer/MainActivity.java
@@ -1,12 +1,21 @@
 package com.jerrylu.supereasyvideoplayer;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +28,35 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = inputText1.getText().toString();
-                Toast.makeText(MainActivity.this,String.format("Hello, %s!",name), Toast.LENGTH_SHORT).show();
+//                String name = inputText1.getText().toString();
+//                Toast.makeText(MainActivity.this,String.format("Hello, %s!",name), Toast.LENGTH_SHORT).show();
+                Intent fileSelectIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                fileSelectIntent.setType("*/*");
+                fileSelectIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(fileSelectIntent,1);
+
+
             }
         });
-
     }
 
+    String path;
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK){
+            Uri uri = data.getData();
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor actualimagecursor = managedQuery(uri, proj, null, null, null);
+            int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            actualimagecursor.moveToFirst();
+            String img_path = actualimagecursor.getString(actual_image_column_index);
+            File file = new File(img_path);
+            Toast.makeText(MainActivity.this, file.toString(), Toast.LENGTH_SHORT).show();
+
+            Intent toVideoPlay = new Intent(MainActivity.this,VideoPlayActivity.class);
+            toVideoPlay.putExtra("url",file.toString());
+            startActivity(toVideoPlay);
+        }
+    }
 }
---
+++ app/src/main/java/com/jerrylu/supereasyvideoplayer/VideoPlayActivity.java
@@ -0,0 +1,30 @@
+package com.jerrylu.supereasyvideoplayer;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class VideoPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_video_play);
+        Intent getIntent = getIntent();
+        String file = getIntent.getStringExtra("url");
+        VideoView videoView = (VideoView)findViewById(R.id.video1);
+        videoView.setVideoURI(Uri.parse(file));
+        videoView.start();
+        Toast.makeText(VideoPlayActivity.this,"Wotest",Toast.LENGTH_SHORT);
+    }
+}
