4a6b89263f3ea6d2dcce877b02a0e13b87057b2b
Jerry Lu <jerrylu.mail@qq.com>
2018-02-17 13:54:11 +0800

    Able to keep play progress when rotate screen

--- app/src/main/java/com/jerrylu/supereasyvideoplayer/VideoPlayActivity.java
+++ app/src/main/java/com/jerrylu/supereasyvideoplayer/VideoPlayActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.MediaController;
@@ -15,19 +16,35 @@ import java.io.File;
 
 public class VideoPlayActivity extends AppCompatActivity {
 
+    private VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // hide title bar and status bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_video_play);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        Intent getIntent = getIntent();
-        String file = getIntent.getStringExtra("url");
-        VideoView videoView = (VideoView)findViewById(R.id.video1);
-        videoView.setVideoURI(Uri.parse(file));
-        videoView.setMediaController(new MediaController(this));
+
+        // get video path and play
+        if(videoView == null){
+            Intent getIntent = getIntent();
+            String file = getIntent.getStringExtra("url");
+            videoView = (VideoView)findViewById(R.id.video1);
+            videoView.setVideoURI(Uri.parse(file));
+            videoView.setMediaController(new MediaController(this));
+        }
+
+        if(savedInstanceState !=null && savedInstanceState.getInt("currentPos") !=0){
+            videoView.seekTo(savedInstanceState.getInt("currentPos"));
+        }
         videoView.start();
-        Toast.makeText(VideoPlayActivity.this,"Wotest",Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("currentPos",videoView.getCurrentPosition());
+        super.onSaveInstanceState(outState);
     }
 }
